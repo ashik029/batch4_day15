6dd00149a1e0ea75e3cd1f1267312faa1e53f162
ashik029 <mrashik029@gmail.com>
2022-02-03 17:20:15 +0600

    day15

---
+++ app/classes/Example.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\classes;
+
+
+class Example
+{
+  public $firstNumber = 10;
+  public $firstName;
+  public $lastName;
+  public function  index ()
+  {
+//      echo "hello world";
+      $this->firstName = "Habibur";
+      $this->lastName = "Rahman";
+      echo $this->firstNumber;
+      echo  '<br/>';
+      echo $this->firstName;
+      echo  '<br/>';
+      echo $this->lastName;
+      echo  '<br/>';
+      echo $this->firstName.$this->lastName;
+  }
+}
